ccca2b17c0b64d5dcddce19f79135e43b8314eec
Pyae Phyo Zaw <hj24976@bristol.ac.uk>
2026-06-29 00:15:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+    } else {
+    document.querySelector("h1").innerHTML = "It's a tie!";
+    }
